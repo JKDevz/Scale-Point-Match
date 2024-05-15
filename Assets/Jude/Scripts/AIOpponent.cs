fac57e27187f4710641b9a40e7d2d495b4a25db7
JKDevs <st10057582@vgconnect.edu.za>
2024-05-15 06:26:41 +0200

    Started Progress on AI

---
+++ Assets/Jude/Scripts/AIOpponent.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIOpponent : MonoBehaviour
+{
+    #region VARIABLES
+
+    [Header("Player References")]
+    public Player aiPlayer;
+
+    [SerializeField] private Node rootNode;
+
+    //Reference Leandro's Tree Node
+
+    #endregion
+
+    #region UNITY METHODS
+
+    private void Awake()
+    {
+        //Set player data
+    }
+
+    #endregion
+
+    #region METHOD
+
+
+
+    #endregion
+}
---
+++ Assets/Jude/Scripts/Classes/Node.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    #region VARIABLES
+
+    public List<Node> children;
+    public Node parent;
+    public int minimaxValue;
+    public bool isMaximiser;
+    public Vector2 movePosition = new Vector2() { x = 0, y = 0};
+    public GameBoard simulation;
+    public int gamePhase;
+
+    #endregion
+}
